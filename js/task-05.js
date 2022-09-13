bfd35dbdc589f58919ec7b2cb1ecb294d27f91e6
al99vin <al99vin1@gmail.com>
2022-09-13 17:02:49 +0300

    Update task-05.js
    не працює умова, якщо зтерти всю інфу

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,5 @@ const inputText = document.querySelector('input')
 const outputText = document.querySelector('span')
 
 
-if(inputText.textContent === 0){outputText.innerHTML = 'Anonymous'}
-inputText.addEventListener('input', () => {if(inputText.textContent === 0){outputText.innerHTML = 'Anonymous'} else{outputText.innerHTML = inputText.value;}})
-inputText.addEventListener('input', () => {if(inputText.textContent === 0){outputText.innerHTML = 'Anonymous'}})
+if(inputText.value === 0){outputText.innerHTML = 'Anonymous'}
+inputText.addEventListener('input', () => {if(inputText.value === 0){outputText.innerHTML = 'Anonymous'} else{outputText.innerHTML = inputText.value;}})
