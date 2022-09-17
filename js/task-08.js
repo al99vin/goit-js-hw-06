7daa8e3c23f793e2691fdbb824b598f02dcd3eea
al99vin <al99vin1@gmail.com>
2022-09-17 16:51:36 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,7 +1,17 @@
-const emailIn = document.querySelector('input.email')
-const passwordIn = document.querySelector('input.password')
+const form = document.forms[0]
+const loginBut = document.getElementsByTagName("button");
+loginBut[0]?.addEventListener('click', (e)=>{
+    e.preventDefault(); 
+    let email = form.elements.email.value;
+    let password = form.elements.password.value;
+    if(email.trim() == '' || password.trim() == ''){alert("Потрібно заповнити усі поля!");return falsew;}
+    let User ={
+        email,
+        password,
+    }
+    console.log(User)
+    form.reset()
+})
 
-let User ={
-    email,
-    password,
-}
+
+
