b03cf8de365dad2520e5425d3b8fc925d25f8cfb
al99vin <al99vin1@gmail.com>
2022-09-18 19:32:14 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,7 +1,13 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
 const body = document.getElementsByTagName('body')
 const button = document.getElementsByTagName('button')
 const span = document.getElementsByTagName('span')
 
 
-button[0].addEventListener('click', ()=>{body[0].style.backgroundColor = '#' + (Math.floor(Math.random() * 16777215).toString(16))
-span[0].innerText = `${body[0]?.style.backgroundColor}`})
+//button[0].addEventListener('click', ()=>{body[0].style.backgroundColor = '#' + (Math.floor(Math.random() * 16777215).toString(16))
+//span[0].innerText = `${body[0]?.style.backgroundColor}`})
+
+button[0].addEventListener('click',() =>{body[0].style.backgroundColor = getRandomHexColor(); span[0].innerText = `${body[0]?.style.backgroundColor}`})
