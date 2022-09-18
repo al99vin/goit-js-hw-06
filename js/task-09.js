e7c3526ac9e292b356414926b66196eae6898bdd
al99vin <al99vin1@gmail.com>
2022-09-18 19:25:06 +0300

    9 - done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,7 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const body = document.getElementsByTagName('body')
+const button = document.getElementsByTagName('button')
+const span = document.getElementsByTagName('span')
+
+
+button[0].addEventListener('click', ()=>{body[0].style.backgroundColor = '#' + (Math.floor(Math.random() * 16777215).toString(16))
+span[0].innerText = `${body[0]?.style.backgroundColor}`})
